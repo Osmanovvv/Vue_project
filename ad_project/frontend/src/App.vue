5f13f6452b90e2658c905f263df6d833f742df47
Osmanovvv <server.osmanov26@mail.ru>
2025-03-22 04:08:19 +0300

    Три шага подключения бокового меню  (drawer)

--- ad_project/frontend/src/App.vue
+++ ad_project/frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
 
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
 
       <v-toolbar-items class="hidden-sm-and-down">
@@ -11,21 +11,23 @@
           <v-icon start icon="mdi-cake-variant"></v-icon>
           Link One
         </v-btn>
-
         <v-btn>Link Two</v-btn>
         <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
-    <v-main>
-      <!-- Content goes here -->
-    </v-main>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
   </v-app>
 </template>
 
-
-
 <script>
-  export default {
+export default {
+  data() {
+    return {
+      drawer: false
+    };
   }
+}
 </script>
