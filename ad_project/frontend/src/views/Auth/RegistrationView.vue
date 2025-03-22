f45fca1b3db15cc945aab9f8c1a57b10f6d71b3c
Osmanovvv <server.osmanov26@mail.ru>
2025-03-22 15:29:15 +0300

    Добавление поля Confirm Password

--- ad_project/frontend/src/views/Auth/RegistrationView.vue
+++ ad_project/frontend/src/views/Auth/RegistrationView.vue
@@ -26,6 +26,15 @@
                 v-model="password"
                 :rules="passwordRules"
               ></v-text-field>
+
+              <v-text-field
+                prepend-icon="mdi-lock"
+                name="confirm-password"
+                label="Confirm Password"
+                type="password"
+                v-model="confirmPassword"
+                :rules="confirmPasswordRules"
+              ></v-text-field>
             </v-form>
           </v-card-text>
 
@@ -51,6 +60,7 @@ export default {
     return {
       email: "",
       password: "",
+      confirmPassword: "",
       valid: false,
       emailRules: [
         (v) => !!v || "E-mail is required",
@@ -59,6 +69,10 @@ export default {
       passwordRules: [
         (v) => !!v || "Password is required",
         (v) => (v && v.length >= 6) || "Password must be more or equal than 6 characters",
+      ],
+      confirmPasswordRules: [
+        (v) => !!v || "Password is required",
+        (v) => v === this.password || "Password should match",
       ],
     };
   },
