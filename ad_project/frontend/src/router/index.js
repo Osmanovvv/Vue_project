bf12cbc29755a481b2f1b4377b1d67ac81e8857d
Osmanovvv <server.osmanov26@mail.ru>
2025-04-06 16:25:48 +0300

    10-11

--- ad_project/frontend/src/router/index.js
+++ ad_project/frontend/src/router/index.js
@@ -1,47 +1,47 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-import HomeView from '../views/HomeView.vue';
-import AdListView from '../views/Ads/AdListView.vue';
-import AdView from '../views/Ads/AdView.vue';
-import NewAdView from '../views/Ads/NewAdView.vue';
-import LoginView from '../views/Auth/LoginView.vue';
-import RegistrationView from '../views/Auth/RegistrationView.vue';
-import OrdersView from '../views/User/OrdersView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AdListView from "../views/Ads/AdListView.vue";
+import AdView from "../views/Ads/AdView.vue";
+import NewAdView from "../views/Ads/NewAdView.vue";
+import LoginView from "../views/Auth/LoginView.vue";
+import RegistrationView from "../views/Auth/RegistrationView.vue";
+import OrdersView from "../views/User/OrdersView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
+    path: "/",
+    name: "home",
     component: HomeView,
   },
   {
-    path: '/ad/:id',
-    name: 'ad',
+    path: "/ad/:id",
+    name: "ad",
+    props: true,
     component: AdView,
   },
   {
-    path: '/list',
-    name: 'list',
+    path: "/list",
+    name: "list",
     component: AdListView,
   },
   {
-    path: '/new',
-    name: 'newAd',
+    path: "/new",
+    name: "newAd",
     component: NewAdView,
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: LoginView,
   },
   {
-    path: '/registration',
-    name: 'reg',
+    path: "/registration",
+    name: "reg",
     component: RegistrationView,
   },
   {
-    path: '/orders',
-    name: 'orders',
+    path: "/orders",
+    name: "orders",
     component: OrdersView,
   },
 ];
